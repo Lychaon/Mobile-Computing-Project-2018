bdfa08b0fd7220d33ee81ef2ec6aae9e7ddc9477
Lychaon <wembleywilliams@gmail.com>
2018-02-08 14:19:36 -0500

    Configured project to use jQuery, added a slide show as the root page, added the index page, added depenedces to app.module.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
   }
+  slides = [
+    {
+      title: "Welcome to the <b>Kingston Tour</b>!",
+      description: "<b>The Kingston Tour App</b> is a small, lightweight application that helps tourists find nearby hotspots and interesting areas.",
+      image: "assets/imgs/slide-1.png"
+    },
+    {
+      title: "Easy To Use",
+      description: "<b>With</b> an intuitive design and a familiar color scheme and layout you'll feel right at home",
+      image: "assets/imgs/slide-2.png"
+    }
+  ];
 
 }
