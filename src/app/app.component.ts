4f6454e1f379795b1c5fbd462864deb878611297
Lychaon <wembleywilliams@gmail.com>
2018-02-08 21:36:56 -0500

    Home page (Landing Page) has been linked to the home button in side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -10,7 +10,8 @@ import { LandingPage } from '../pages/landing/landing';
   templateUrl: 'app.html'
 })
 export class MyApp {
-   
+  @ViewChild(Nav) nav: Nav;
+
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -21,5 +22,11 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  loadHome()
+  {
+    this.nav.setRoot(LandingPage);
+  }
+
 }
 
